32c50b30aa06eb9fd1f75879b0c35eef13548f07
shahnupur1901 <67814929+shahnupur1901@users.noreply.github.com>
2021-05-12 12:54:15 +0530

    Update main.cpp

--- VaccineReservationProject/main.cpp
+++ VaccineReservationProject/main.cpp
@@ -12,33 +12,42 @@ int main(){
 	Citizen c;
 	Government govt;
 	int option = 0;
-
 	do{
-		cout<<"Enter your choice : "<<endl;
-		cout<<"1 - Select if you are a government administrator for information updation.."<<endl;
+		cout<<"1 - Government Interface."<<endl;
 		cout<<"2 - Register for a vaccination."<<endl;
 		cout<<"3 - Delete your reservation."<<endl;
 		cout<<"4 - If you've received the vaccination, then update the status."<<endl;
 		cout<<"5 - Check the status of your vaccination."<<endl;
-		cout<<"6 - Select if you are a government administrator for slot modification."<<endl;
-		cout<<"7 - Exit."<<endl;
+		cout<<"6 - Exit."<<endl;
+		cout<<"Enter your choice : "<<endl;
 		int option; cin>>option;
 		switch(option){
-		case 1 : govt.govtInterface();break;
-		case 2 : govt.reserveVaccine(); break;
-		case 3 : govt.deleteVaccine(); break;
-		case 4 :{
-			cout<<"Enter yes if you have received your vaccine, else enter no."<<endl;
-			string s; cin>>s;
-			if(s=="yes")
-				govt.modify();
-			break;
+		case 1 : {
+			int c = 0;
+			cout<<"1. Update the number of vaccines to be provided.\n2. Display the vaccinated count.\n3. Exit "<<endl;
+			cout<<"Enter your choice : "<<endl;
+			cin>>c;
+			switch(c){
+			case 1: govt.govtInterface();break;
+			case 2: govt.modifyEndOfDay();break;
+			default:break;
 			}
+		}
+		break;
+		case 2 : govt.reserveVaccine(); break;
+		case 3 : govt.cancelReservation(); break;
+		case 4 :{
+			cout<<"Select your status: \n1. Vaccination received.\n2. Vaccination not received.\n3. Exit. "<<endl;
+						int ch; cin>>ch;
+						switch(ch){
+						case 1: govt.modify();break;
+						default : break;
+						}
+						}
 		case 5 : govt.checkStatus();break;
-		case 6 : govt.modifyEndOfDay();break;
-		case 7 : break;
+		case 6 : break;
 		default :cout<<"Invalid choice. Kindly enter your choice again."<<endl; break;
 		}
-	}while(option!=7);
+	}while(option!=6);
 	cout<<"Have a nice day!"<<endl;
 }
